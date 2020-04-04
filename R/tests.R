edeb1e2852e1016c1896aad674b8eb59328e4078
Luis Cano <luiscanoayestas@gmail.com>
2020-04-04 12:38:54 +0200

    travis added

--- R/tests.R
+++ R/tests.R
@@ -1,8 +1,10 @@
 library(testthat)
 
 
-test_that("fars_read"{
+test_that("fars_read", {
   filename("accident_2013.csv.bz2")
   expect_that("accident_2013.csv.bz2", is.data.frame("data.frame"))
 
 })
+
+#test
